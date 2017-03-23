7e9449ad8c3d37b21021724733bb753d7910c102
Jesse Hull <jessehull@gmail.com>
2017-03-23 16:44:10 -0700

    take original args or sql

--- src/index.js
+++ src/index.js
@@ -9,18 +9,23 @@ class SnowflakeConnectionPromisified {
     this.destroyAsync = q.nbind(this.connection.destroy, this.connection)
   }
 
-  executeAsync(sqlStatement) {
+  executeAsync(sqlStatementOrParams) {
     return q.Promise((resolve, reject) => {
-      this.connection.execute({
-        sqlText: sqlStatement,
-        complete: (err, stmt, rows) => {
-          if (err) {
-            reject(err)
-            return
-          }
-          resolve({stmt: stmt, rows: rows})
+      const complete = (err, stmt, rows) => {
+        if (err) {
+          reject(err)
+          return
         }
-      })
+        resolve({stmt: stmt, rows: rows})
+      }
+      let params = {}
+      if (_.isString(sqlStatementOrParams)) {
+        params.sqlText = sqlStatementOrParams
+      } else {
+        params = _.clone(sqlStatementOrParams)
+      }
+      params.complete = complete
+      this.connection.execute(params)
     })
   }
 }
